dd04d9641b0d0dd5d844979535d916d119feb76e
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-01-07 15:35:42 +0900

    delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const googlehome = require('./google-home-notifier');
-const language = 'ja';
-
-googlehome.device('Google-Home', language);
-
-googlehome.notify('こんにちは', function(res) {
-  console.log(res);
-});
- 
